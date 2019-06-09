a99c4dac69dfa7ee2e9a24850d42fc2a18901c0f
Glitch (flask-python3) <none>
2019-06-09 13:30:03 +0000

    🚗🚈 Checkpoint ./static/index.html:620424/1610 ./server.py:620424/465

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -10,6 +10,14 @@ socketio = SocketIO(app)
 def hello():
   return send_from_directory('static', 'index.html')
 
+DREAMS = []
+
+@socketio.on('dream')
+def handle_dream(json):
+    DREAMS.append(json['dream'])
+    print(json)
+    emit('dream', DREAMS)
+    
 
 if __name__ == '__main__':
     socketio.run(app)
