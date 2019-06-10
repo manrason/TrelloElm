344e070b87841e95508af66a62714dd3acfb811e
Glitch (flask-python3) <none>
2019-06-10 18:50:20 +0000

    👸🎰 Checkpoint ./sh/install.sh:620424/225

--- sh/install.sh
+++ sh/install.sh
@@ -1,5 +1,4 @@
 elm_bin_url="https://github.com/elm/compiler/releases/download/0.19.0/binaries-for-linux.tar.gz"
-elmformat_bin_url="https://github.com/avh4/elm-format/releases/download/0.8.1/elm-format-0.8.1-linux-x64.tgz"
 
 echo "Grab all python dependencies..."
 pip3 install --user --upgrade pip >/dev/null
@@ -18,6 +17,7 @@ else
 fi
 
 # experiment for using elm-format but it does not play well with glitch (need to "refresh" after each elm-format...)
+#elmformat_bin_url="https://github.com/avh4/elm-format/releases/download/0.8.1/elm-format-0.8.1-linux-x64.tgz"
 #echo ""
 #if [ ! -f "/app/.local/bin/elm-format" ]; then
 #   echo "Elm-form doesn't seem installed, I'm downloading it..."
