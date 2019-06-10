4f1e2202cf2d8cf81ca0c47a1bc059664fca47ff
Glitch (flask-python3) <none>
2019-06-10 13:58:17 +0000

    🐔🛳 Checkpoint ./src/Main.elm:620424/847 ./sh/install.sh:620424/178 ./sh/start.sh:620424/355 ./server.py:620424/20 ./glitch.json:620424/10

--- sh/install.sh
+++ sh/install.sh
@@ -1,10 +1,13 @@
 elm_bin_url="https://github.com/elm/compiler/releases/download/0.19.0/binaries-for-linux.tar.gz"
 
 
-pip3 install --upgrade pip
-pip3 install --user -r requirements.txt
+echo "Grab all python dependencies..."
+pip3 install --user --upgrade pip >/dev/null
+pip3 install --user  -r requirements.txt > /dev/null
+echo "Python deps grabbed!"
 
-if [! -f "/app/.local/bin/elm"]; then
+echo ""
+if [ ! -f "/app/.local/bin/elm" ]; then
    echo "Elm doesn't seem installed, I'm downloading it..."
    cd /app/.local/bin/
    
--- sh/start.sh
+++ sh/start.sh
@@ -1,3 +1,12 @@
+elm make src/Main.elm --output public/elm.js
+
 PYTHONUNBUFFERED=true python3 server.py
 
-elm make src/Main.elm --output public/elm.js
+
+#  let's start a basic webserver to stop Glitch from constantly restarting
+source ${APP_TYPES_DIR}/utils.sh
+export PATH="${PATH}:${DEFAULT_NODE_DIR}"
+ws --port 3000 --log.format combined --directory . --blacklist '/.env' '/.data' '/.git' &
+pid=$!
+
+wait ${pid}
