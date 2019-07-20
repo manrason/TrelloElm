e06cb23419cd0b68f1b3ce3dc76a6d83f2f94d51
Glitch (flask-python3) <none>
2019-07-20 14:13:48 +0000

    👶🚶 Checkpoint ./models/user.py:620424/1796

--- models/user.py
+++ models/user.py
@@ -1,4 +1,31 @@
 import flask_login
 
 class User(flask_login.UserMixin):
-    pass
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.id = None
+
+    def save(self, cursor):
+        if self.id is None:
+            cursor.execute('''
+              INSERT INTO users 
+              ( name
+              , email
+              )
+              VALUES 
+              (?, ?)
+            ''', (self.name, self.email)
+            )
+            
+    @classmethod
+    def create_table(cls, cursor):
+        cursor.execute('DROP TABLE IF EXISTS shortcuts')
+
+        cursor.execute('''
+        CREATE TABLE users
+        ( id INT PRIMARY KEY NOT NULL AUTO_INCREMENT
+        , name TEXT NOT NULL
+        , email TEXT NOT NULL
+        )''')
+
